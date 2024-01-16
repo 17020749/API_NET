1675312b2d702cffe6790d7bf392dc117f0ab79b
Nguyen Trung Hieu <71979577+17020749@users.noreply.github.com>
2024-01-16 10:52:35 +0700

    how to create and connect database

--- obj/Debug/net7.0/API_CORE.AssemblyInfo.cs
+++ obj/Debug/net7.0/API_CORE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_CORE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c39eee94357459a3ef2706c23c1f8e9dd81af08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875d1ea477678149d9ace1a48afdc64d16d44185")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_CORE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_CORE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
